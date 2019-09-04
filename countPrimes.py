6680bce59e8f5103bc309ea6a850dd0cc93025c3
ethan-marsh <emarsh55@gmail.com>
2019-09-04 11:36:04 -0700

    Count primes (sieving)

---
+++ countPrimes.py
@@ -0,0 +1,57 @@
+# 204. Count Primes
+# Count the number of prime numbers less than a non-negative number, n.
+# Example:
+# Input: 10
+# Output: 4
+# Explanation:
+# There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+
+class Solution:
+    def countPrimesSimple(self, n: int) -> int:
+        """
+        Time to sift out multiples of p is proportional to n/p,
+        so overall = O(n/2 + n/3 + n/5 + n/7, ...) -> O(n log log n)
+        Space: O(n), which is the biggest problem with simple sieve.
+        """
+        if n < 2:
+            return 0
+
+        primes = []
+        is_prime = [False, False] + [True for i in range(n-1)]
+
+        for x in range(2, n):
+            if is_prime[x]:
+                primes.append(x)
+            for i in range(x*2, n, x):
+                is_prime[i] = False
+
+        return len(primes)
+
+    def countPrimesOptimized(self, n: int) -> int:
+        """
+        Asymptiotic time and space coplexity are same for basic sieving,
+        but practical improvement to runtime by sieving p's multiples
+        from p^2 instead of p, since all numbers of the
+        form `kp` where k < p have already been sieved out.
+        Storage is also be reduced by ignoring even numbers.
+        """
+        if n < 2:
+            return 0
+
+        size = (n - 3) // 2 + 1
+        primes = [2]
+        is_prime = [True for i in range(size)]  # represents if (2i+3) is prime
+
+        for i in range(size):
+            if is_prime[i]:
+                p = i * 2 + 3
+                primes.append(p)
+                # Sieve from p^2, where p^2 = (4i^2 + 12i + 9).
+                # Index in is_prime is (2i^2 + 6i + 3)
+                # because is_prime[i] = 2i + 3.
+                # Note: Use long for `j` since p^2 may overflow.
+                for j in range(2 * i**2 + 6 * i + 3, size, p):
+                    is_prime[j] = False
+
+        return len(primes)
